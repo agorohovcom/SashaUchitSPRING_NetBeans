ac434e67072ee818d10a64059ed9f92d5618276d
agorohov <aleksandr.gorohov@gmail.com>
2023-04-04 10:14:54 +0300

    Конфигурация с помощью Java кода, способ 1

---
+++ src/main/java/com/agorohov/spring/sashaUchitSPRING_NetBeans/spring_introduction/MyConfig.java
@@ -0,0 +1,11 @@
+package com.agorohov.spring.sashaUchitSPRING_NetBeans.spring_introduction;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.agorohov.spring.sashaUchitSPRING_NetBeans.spring_introduction")
+//@ComponentScan("personBeanspring_introduction")  так не работает (в IntellijIDEA работает), надо указывать полный путь
+public class MyConfig {
+    
+}

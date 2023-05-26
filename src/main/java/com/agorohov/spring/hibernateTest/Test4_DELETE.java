e93219ef0ba5a3fbb66c941d6d42cd9b9c3762ae
agorohov <aleksandr.gorohov@gmail.com>
2023-05-26 16:29:41 +0300

    Разобрал UPDATE и DELETE

---
+++ src/main/java/com/agorohov/spring/hibernateTest/Test4_DELETE.java
@@ -0,0 +1,34 @@
+package com.agorohov.spring.hibernateTest;
+
+import com.agorohov.spring.hibernateTest.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class Test4_DELETE {
+
+    public static void main(String[] args) {
+        SessionFactory factory = new AnnotationConfiguration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            // пример 1
+//            Employee emp = (Employee) session.get(Employee.class, 1);
+//            session.delete(emp);
+            
+            // пример 2
+            session.createQuery("delete Employee where name = 'Elena'")
+                    .executeUpdate();
+
+            session.getTransaction().commit();
+            System.out.println("Done! =)");
+        } finally {
+            factory.close();
+        }
+    }
+}
---
+++ src/main/java/com/agorohov/spring/hibernateTest/Test4_UPDATE.java
@@ -0,0 +1,33 @@
+package com.agorohov.spring.hibernateTest;
+
+import com.agorohov.spring.hibernateTest.entity.Employee;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class Test4_UPDATE {
+
+    public static void main(String[] args) {
+        SessionFactory factory = new AnnotationConfiguration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+//            Employee emp = (Employee) session.get(Employee.class, 1);
+//            emp.setSalary(1000);
+
+            session.createQuery("update Employee set salary = 1000 where name = 'Aleksandr'")
+                    .executeUpdate();
+
+            session.getTransaction().commit();
+            System.out.println("Done! =)");
+        } finally {
+            factory.close();
+        }
+    }
+}
